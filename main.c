f8baee622c96d90db4adfe73316e4d669e1e53b1
Rafael Santos <15151000+Blackmagicbox@users.noreply.github.com>
2024-04-29 03:22:55 +0200

    FEAT Background Image

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdbool.h>
 
 #define WINDOW_WIDTH 1280
@@ -10,6 +11,7 @@ int main(void) {
     printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
     return 1;
   }
+  IMG_Init(IMG_INIT_PNG);
 
   SDL_Window* window = SDL_CreateWindow(
       "Casteroids",
@@ -30,6 +32,18 @@ int main(void) {
   );
 
   // Create a background Image
+  SDL_Surface* background_surface = IMG_Load_RW(
+      SDL_RWFromFile("./graphics/background.png", "rb"),
+      1
+    );
+  if (background_surface == NULL) {
+    printf("Could not load background image: %s\n", IMG_GetError());
+  }
+  SDL_Texture* background_texture = SDL_CreateTextureFromSurface(renderer, background_surface);
+  SDL_FreeSurface(background_surface);
+  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+
 
 
   bool running = true;
@@ -40,8 +54,11 @@ int main(void) {
         running = false;
       }
     }
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 12, 2, 26, 255);
     SDL_RenderClear(renderer);
+    SDL_Rect background_rect = {0,0, WINDOW_WIDTH, WINDOW_HEIGHT};
+    SDL_SetTextureAlphaMod(background_texture, 180);
+    SDL_RenderCopy(renderer, background_texture, NULL, &background_rect);
     SDL_RenderPresent(renderer);
   }
 
